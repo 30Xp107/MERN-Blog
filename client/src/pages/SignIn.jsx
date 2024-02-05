29ed4a2c4de28ca22f3d64011987c1019cef520c
30Xp107 <jhanbrent0630@gmail.com>
2024-02-05 14:43:57 +0000

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,24 +1,25 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js'
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  const {loading, error: errorMessage} = useSelector((state) => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() })
   } 
   const handleSubmit = async(e) => {
     e.preventDefault()
     if(!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields')
+      return dispatch(signInFailure('Please fill out all fields'))
     }
     try {
-      setLoading(false)
-      setErrorMessage(null)
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -26,15 +27,14 @@ export default function SignIn() {
       })
       const data = await res.json()
       if(data.success === false) {
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message))
       }
-      setLoading(false)
       if(res.ok) {
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false)
+      dispatch(signInFailure(error.message))
     }
   }
 
@@ -67,7 +67,7 @@ export default function SignIn() {
                     <Spinner size='sm'  />
                     <span className='pl-3'>Loading...</span>
                   </>
-                ) : 'Sign Up'
+                ) : 'Sign In'
               }
             </Button>
             <div className="flex gap-2 text-sm mt-5">
